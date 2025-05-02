a1e56a85bb9e1841ad6302f1126c1db279e196e8
karol-soup <Karol Suarez@DESKTOP-ONS08P7>
2025-05-02 12:50:03 -0700

    Removed build and out folders from tracking

--- getBreath1/out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Karol Suarez/Desktop/getBreath1/getBreath1
-# Build directory: C:/Users/Karol Suarez/Desktop/getBreath1/getBreath1/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[calculatorTests]=] "C:/Users/Karol Suarez/Desktop/getBreath1/getBreath1/out/build/x64-Debug/test_exec.exe")
-set_tests_properties([=[calculatorTests]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Karol Suarez/Desktop/getBreath1/getBreath1/CMakeLists.txt;28;add_test;C:/Users/Karol Suarez/Desktop/getBreath1/getBreath1/CMakeLists.txt;0;")
-subdirs("extern/googletest")
--- getBreath1/out/build/x64-Debug/extern/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Karol Suarez/Desktop/getBreath1/getBreath1/extern/googletest
-# Build directory: C:/Users/Karol Suarez/Desktop/getBreath1/getBreath1/out/build/x64-Debug/extern/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
