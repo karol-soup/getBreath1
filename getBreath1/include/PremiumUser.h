02ef8a05cacd3823605daf2eac60e3d03eaf8651
karol-soup <Karol Suarez@DESKTOP-ONS08P7>
2025-05-23 13:50:24 -0700

    overrided setCycle

--- getBreath1/include/PremiumUser.h
+++ getBreath1/include/PremiumUser.h
@@ -4,10 +4,9 @@
 class PremiumUser : public User { // Inherits all methods and attributes of User
 public:
     // Default constructor for BasicUser
-    PremiumUser() : User() {
-        cycleCount = 5; //sets the specific cycle count for BasicUser
-    }
+    PremiumUser() : User() {}
 
     // overrides base class to show basic user
+    void setCycle() override;
     void displayInfo() const override;
 };
--- getBreath1/src/PremiumUser.cpp
+++ getBreath1/src/PremiumUser.cpp
@@ -2,6 +2,10 @@
 #include<iostream>
 using namespace std;
 
+void PremiumUser::setCycle() {
+	cout << "Cycles with basic getBreath subscrition is up to 10.\n";
+	getCycle();
+}
 void PremiumUser::displayInfo() const {
     User::displayInfo(); // base class's displayInfo
     cout << "User Type: Premium\n";
