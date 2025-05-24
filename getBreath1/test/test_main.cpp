61749a7bfc12e45e957491b2517e4df102516e5e
karol-soup <Karol Suarez@DESKTOP-ONS08P7>
2025-05-23 23:41:52 -0700

    Implemented exceptions

--- getBreath1/test/test_main.cpp
+++ getBreath1/test/test_main.cpp
@@ -1,29 +1,35 @@
 
 #include <gtest/gtest.h>
-#include"../include/getFunctions.h"
-#include"../include/beginBreathing.h"
-#include"../include/logUser.h"
-#include"../include/UserInfo.h"
+#include <stdexcept>
+#include"../include/User.h"
 #include"../include/Date.h"
-#include"../include/Mood.h"
+#include"../include/Utils.h"
 
-
-TEST(getFunctionsTest, validMoodCheck) {
+TEST(getFunctionsTest, invalidMoodCheck_Exceptions) {
+    EXPECT_THROW(isValidMood(-23), std::out_of_range);
+    EXPECT_THROW(isValidMood(6), std::out_of_range);
+}
+TEST(getFunctionsTest, validMoodCheck_Exceptions) {
     EXPECT_TRUE(isValidMood(1));
-    EXPECT_FALSE(isValidMood(0));
+    EXPECT_TRUE(isValidMood(4));
+}
+
+TEST(getFunctionsTest, invalidCycle_Exceptions) {
+    EXPECT_THROW(isValidCycle(-23), std::out_of_range);
+    EXPECT_THROW(isValidCycle(0), std::out_of_range);
 }
 
 TEST(getFunctionsTest, validCycleCheck) {
     EXPECT_TRUE(isValidCycle(3));
-    EXPECT_FALSE(isValidCycle(20));
+    EXPECT_TRUE(isValidCycle(1));
 }
 
+TEST(getFunctionsTest, invalidNameCheck_Exceptions) {
+    EXPECT_THROW(isValidName("thisismorethat35wordsidontwantthisinputtobevalidtoolong"),std::invalid_argument);
+}
 TEST(getFunctionsTest, validNameCheck) {
-    EXPECT_TRUE(isValidName("karol"));
-    EXPECT_FALSE(isValidName(""));
-    EXPECT_FALSE(isValidName("thisismorethat35wordsidontwantthisinputtobevalidtoolong"));
+    EXPECT_TRUE(isValidName("Karol"));
 }
-
 
 int main(int argc,char**argv) {
 	::testing::InitGoogleTest(&argc, argv);
